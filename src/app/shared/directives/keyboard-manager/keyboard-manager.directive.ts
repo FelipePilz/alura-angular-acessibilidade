78e14d299a2a9180ebda81c0ea407461af457804
FelipePilz <felipevilvertpilz@gmail.com>
2021-11-25 20:54:57 -0300

    Criando um Keyboard ManagedItem Directive

--- src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
+++ src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
@@ -1,13 +1,23 @@
-import { Directive, HostListener } from '@angular/core';
+import { KeyboardManagedItemDirective } from './keyboard-managed-item.directive';
+import {
+  ContentChildren,
+  Directive,
+  HostListener,
+  QueryList,
+} from '@angular/core';
 
 @Directive({
   selector: '[appKeyboardManager]',
 })
 export class KeyboardManagerDirective {
+  @ContentChildren(KeyboardManagedItemDirective)
+  public items: QueryList<KeyboardManagedItemDirective> = null;
+
   @HostListener('keyup', ['$event'])
   public manageKeys(event: KeyboardEvent): void {
     switch (event.key) {
       case 'ArrowUp':
+        console.log(this.items);
         console.log('up');
         break;
 
